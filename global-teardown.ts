7a2c1af826a98f301af4d9c914a96c12f653fe1a
ramojirao9152 <140970503+ramojirao9152@users.noreply.github.com>
2026-03-04 22:34:02 +0530

    Comment out sendEmail call in globalTeardown
    Comment out the sendEmail function call in globalTeardown.

--- global-teardown.ts
+++ global-teardown.ts
@@ -3,5 +3,5 @@ import { sendEmail } from './notifyEmail';
 
 export default async function globalTeardown() {
   console.log('📧 Sending automation report email...');
-  await sendEmail();
+  // await sendEmail();
 }
